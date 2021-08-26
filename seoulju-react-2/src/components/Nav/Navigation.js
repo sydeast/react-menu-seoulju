455fbe2dd12e10d1c14339292f147b6af15f651f
Syd <24235464+sydeast@users.noreply.github.com>
2021-08-26 01:21:39 -0500

    Update the nav cart to display number of items

--- seoulju-react-2/src/components/Nav/Navigation.js
+++ seoulju-react-2/src/components/Nav/Navigation.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 // import { Link } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import Header from '../Header';
 import styles from './Navbar.module.css';
+import {connect } from 'react-redux'
 
 
-const Navigation = () => {
+
+const Navigation = ({order}) => {
+  const [itemCount, setItemCount] = useState(0); //set the cart counts to zero intially
+
+  useEffect(() => {
+    let count = 0;
+    //iterate over the order array and count each ordered item
+    order.forEach((item) => {
+      count += item.ordered;
+    });
+      
+    setItemCount(count);
+  }, [order, itemCount]);
+
   return (
     <Navbar bg='light' expand='lg' sticky='top' className={styles.navbar}>
       <Container>
@@ -14,13 +28,18 @@ const Navigation = () => {
         </Navbar.Brand>
         <div className={styles.navbar__cart}>
           <Nav className='me-auto'>
-            <Nav.Link href='checkout'>Cart</Nav.Link>
-            <div className={styles.cart__counter}>0 Items</div>
+            <Nav.Link href='checkout'>Cart:</Nav.Link>
+            <div className={styles.cart__counter}>{itemCount} Items</div>
           </Nav>
         </div>
       </Container>
     </Navbar>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+    order: state.order
+  };
+};
 
-export default Navigation;
+export default connect(mapStateToProps) (Navigation);
