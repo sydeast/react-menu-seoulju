8f958c029ba9fc9fb1a1aa152b1659cae01a0743
Syd <24235464+sydeast@users.noreply.github.com>
2021-08-26 10:54:53 -0500

    Fix order totals to display in the cart and css

--- seoulju-react-2/src/containers/CartContainer.js
+++ seoulju-react-2/src/containers/CartContainer.js
@@ -1,28 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Order from '../components/Order';
+import styles from './Cart.module.css';
 // import { Link } from 'react-router-dom';
 
 function Cart({order}) {
-          return (
-          <div>
-            <div className='cart-list'>
-              {order.map((item) => (
-                <Order key={item.id} item={item} />
-              ))}
-            </div>
-            <div className='cart-sum'>
-              <h4 className='cart-title'>Cart Summary</h4>
-              <div className='item.price'>
-                <span>TOTAL: (7 items)</span>
-                <span>$ 0</span>
-              </div>
-              <button className='summary__checkoutBtn'>
-                Proceed To Checkout
-              </button>
-            </div>
-          </div>
-        );
+
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+
+    useEffect(() => {
+      let items = 0;
+      let price = 0;
+
+      order.forEach((item) => {
+        items += item.ordered;
+        price += item.ordered * item.price;
+      });
+
+      setTotalItems(items);
+      setTotalPrice(price);
+    }, [order, totalPrice, totalItems, setTotalPrice, setTotalItems]);
+
+
+  return (
+    <div className={styles.order}>
+      <div className={styles.order_items}>
+        {order.map((item) => (
+          <Order key={item.id} item={item} />
+        ))}
+      </div>
+      <div className={styles.order_summary}>
+        <h4 className={styles.summary_title}>Order Summary</h4>
+        <div className={styles.summary_price}>
+          <span>TOTAL: ({totalItems} items)</span>
+          <span>$ {totalPrice}</span>
+        </div>
+        <button className='summary__checkoutBtn'>Checkout</button>
+      </div>
+    </div>
+  );
 
 };
 
